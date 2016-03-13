1b5c6ab4a5bbb93bc9ed110822f43e0901e5d6dd
ghostflying <GhostFlying@Gmail.com>
2016-03-13 14:38:11 +0800

    try to work in Android N

--- app/src/main/java/com/ghostflying/locationreportenabler/PropUtil.java
+++ app/src/main/java/com/ghostflying/locationreportenabler/PropUtil.java
@@ -7,12 +7,14 @@ import java.io.IOException;
  * Created by ghostflying on 3/18/15.
  */
 public final class PropUtil {
-    private static final String COMMAND_PREFIX = "setprop ";
+//    private static final String COMMAND_PREFIX = "setprop ";
+    private static final String COMMAND_PREFIX = "runcon u:r:radio:s0 setprop ";
     private static final String[] PROPERTIES = {"gsm.sim.operator.numeric 310030"};
+    private static final String COMMAND_SU = "su";
 
     public static void enableLocationReport() {
         try{
-            Process p = Runtime.getRuntime().exec("su");
+            Process p = Runtime.getRuntime().exec(COMMAND_SU);
             DataOutputStream os = new DataOutputStream(p.getOutputStream());
             for (String property : PROPERTIES) {
                 os.writeBytes(COMMAND_PREFIX + property + "\n");
